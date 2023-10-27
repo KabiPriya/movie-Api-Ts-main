bdcd265a349d01baf764c0b6faf1dee2146d0299
KabiPriya <kabipriya.t@gmail.com>
2023-10-27 20:55:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# MOVIE WEBSITE
+
+Add:
+• To Add Moviecard with title and released year.
+Edit and Delete:
+• To edit and delete movie card.
+
+# TYPESCRIPT
+
+• TypeScript provides various other features, like interfaces, type aliases, abstract classes, function overloading, tuple, generics, etc.
---
+++ app/src/App.tsx
@@ -0,0 +1,31 @@
+import "@picocss/pico";
+import { Suspense, lazy, useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import { IMovie } from "./type";
+// import Add from "./pages/Add";
+// import Edit from "./pages/Edit";
+
+const Add = lazy(() => import("./pages/Add"));
+const Edit = lazy(() => import("./pages/Edit"));
+
+function App() {
+  const [movie, setMovie] = useState<IMovie>({
+    id: 0,
+    title: "",
+    year: 0,
+  });
+  return (
+    <Suspense>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home handleEdit={(m) => setMovie(m)} />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/edit/:id" element={<Edit movie={movie} />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
+
+export default App;
